8a45d93bd3282f7ffb47bb74efcfbf64f3258d8b
KaelinM <kaelinmm26@ausdk12.org>
2024-08-23 17:04:40 +0000

    s1-w2

--- BodyOfWater.java
+++ BodyOfWater.java
@@ -1,1 +1,59 @@
+public class BodyOfWater{
+    private String name;
+    private int largestDiameter;
+    private double avgDepth;
+    private boolean isSaltWater;
 
+    public BodyOfWater(String n, int l, double a, boolean s){
+        name = n;
+        largestDiameter = l;
+        avgDepth = a;
+        isSaltWater = s;
+    }
+
+    public BodyOfWater(String n, double a){
+        name = n;
+        avgDepth = a;
+    }
+
+    public void setName(String n){
+        name = n;
+    }
+
+    public void setlargestDiameter(int d){
+        largestDiameter = d;
+    }
+
+    public void setavgDepth(double a){
+        avgDepth = a;
+    }
+
+    public void setisSaltWater(boolean s){
+        isSaltWater = s;
+    }
+
+    public String name(){
+        return name;
+    }
+
+    public int largestDiameter(){
+        return largestDiameter;
+    }
+
+    public double avgDepth(){
+        return avgDepth;
+    }
+
+    public boolean isSaltWater(){
+        return isSaltWater;
+    }
+
+    public void printState(){
+        System.out.println("Name:" + name);
+        System.out.println("Largest Diameter:" + largestDiameter);
+        System.out.println("Average Depth:" + avgDepth);
+        System.out.println("Salt Water? :" + isSaltWater);
+    }
+
+
+}
--- BodyOfWaterMain.java
+++ BodyOfWaterMain.java
@@ -1,6 +1,35 @@
 public class BodyOfWaterMain {
     public static void main(String[] args) {
-        BodyOfWater b = new BodyOfWater();
-        System.out.println("here");
+        BodyOfWater b = new BodyOfWater("Lake Powell", 100, 25, true);
+      
+        System.out.println("Name:" + b.name());
+        System.out.println("Largest Diameter:" + b.largestDiameter() + " feet");
+        System.out.println("Average Depth:" + b.avgDepth() + " feet");
+        System.out.println("Salt Water? :" + b.isSaltWater());
+
+        //b.setName("Lake Tahoe");
+        //b.setlargestDiameter(50);
+        //b.setavgDepth(1000);
+        //b.setisSaltWater(false);
+
+        BodyOfWater b1 = new BodyOfWater("Lake Tahoe", 25);
+        
+
+        System.out.println("Name:" + b1.name());
+        System.out.println("Largest Diameter:" + b1.largestDiameter() + " feet");
+        System.out.println("Average Depth:" + b1.avgDepth() + " feet");
+        System.out.println("Salt Water? :" + b1.isSaltWater());
+
+        b.setName("Pacific Ocean");
+        System.out.println("Name changed to:" + b.name());
+
+
+
+
+        //BodyOfWater b1 = new BodyOfWater("Lake Tahoe", 25);
+        //b1.printState();
+
+
     }
+
 }
